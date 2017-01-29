984ee7255cb487e085b6f75466ed351edd26d085
mozolevsky <alf.rulit@gmail.com>
2017-01-29 20:42:08 +0300

    Made esLint test, fixed bugs: in functions replaced this at argument, was separated some event handlers.

--- js/form.js
+++ js/form.js
@@ -1,25 +1,25 @@
-"use strict"
+'use strict';
 
 function getElement(selector) {
   return document.querySelector(selector);
 }
 
-var uploadForm = getElement("#upload-select-image");
-var inputUploadFile = getElement("#upload-file");
-var croppingImgFormWrapper = getElement(".upload-overlay");
-var croppingImgForm = getElement("#upload-filter");
-var uploadFormCancel = getElement(".upload-form-cancel");
+var uploadForm = getElement('#upload-select-image');
+var inputUploadFile = getElement('#upload-file');
+var croppingImgFormWrapper = getElement('.upload-overlay');
+var croppingImgForm = getElement('#upload-filter');
+var uploadFormCancel = getElement('.upload-form-cancel');
 
-var mainImage = getElement(".filter-image-preview");
-var mainImageScaleBlock = getElement(".upload-resize-controls-value");
-var mainImageScale = parseInt(mainImageScaleBlock.getAttribute("value"));
+var mainImage = getElement('.filter-image-preview');
+var mainImageScaleBlock = getElement('.upload-resize-controls-value');
+var mainImageScale = parseInt(mainImageScaleBlock.getAttribute('value'), 10);
 
-function showElement() {
-  this.classList.remove("invisible");
+function showElement(element) {
+  element.classList.remove('invisible');
 }
 
-function hideElement() {
-  this.classList.add("invisible");
+function hideElement(element) {
+  element.classList.add('invisible');
 }
 
 function getFilterName(attr) {
@@ -28,48 +28,44 @@ function getFilterName(attr) {
 
 function reduceMainImg() {
   if (mainImageScale > 25) {
-    mainImageScale -=25;
-    mainImageScaleBlock.value = mainImageScale + "%";
-    mainImage.style.transform = "scale(" + mainImageScale * 0.01 + ")";
+    mainImageScale = mainImageScale - 25;
+    mainImageScaleBlock.value = mainImageScale + '%';
+    mainImage.style.transform = 'scale(' + mainImageScale * 0.01 + ')';
   }
 }
 
 function enlargeMainImg() {
   if (mainImageScale < 100) {
-    mainImageScale +=25;
-    mainImageScaleBlock.value = mainImageScale + "%";
-    mainImage.style.transform = "scale(" + mainImageScale * 0.01 + ")";
+    mainImageScale += 25;
+    mainImageScaleBlock.value = mainImageScale + '%';
+    mainImage.style.transform = 'scale(' + mainImageScale * 0.01 + ')';
   }
 }
 
-inputUploadFile.addEventListener("change", function () {
-  hideElement.call(uploadForm);
-  showElement.call(croppingImgFormWrapper);
+inputUploadFile.addEventListener('change', function () {
+  hideElement(uploadForm);
+  showElement(croppingImgFormWrapper);
 });
 
-croppingImgForm.addEventListener("click", function (event) {
+uploadFormCancel.addEventListener('click', function () {
+  hideElement(croppingImgFormWrapper);
+  showElement(uploadForm);
+});
+
+croppingImgForm.addEventListener('click', function (event) {
   var target = event.target;
 
-  if (target == uploadFormCancel) {
-    hideElement.call(croppingImgFormWrapper);
-    showElement.call(uploadForm);
-  }
-
-  if (~target.className.indexOf("upload-filter-preview")) {
+  if (~target.className.indexOf('upload-filter-preview')) {
     var attr = target.parentElement.getAttribute('for');
     var filterName = getFilterName(attr);
-    mainImage.className = "filter-image-preview " + filterName;
+    mainImage.className = 'filter-image-preview ' + filterName;
   }
 
-  if (~target.className.indexOf("upload-resize-controls-button-dec")) {
+  if (~target.className.indexOf('upload-resize-controls-button-dec')) {
     reduceMainImg();
   }
 
-  if (~target.className.indexOf("upload-resize-controls-button-inc")) {
-    enlargeMainImg()
+  if (~target.className.indexOf('upload-resize-controls-button-inc')) {
+    enlargeMainImg();
   }
-
 });
-
-
-
